5d8c7cdc5547db103c6b6dc43d2b109ba0d0c55a
Aiden Pickett <Ka7nqk@gmail.com>
2024-10-23 14:32:06 -0600

    again

---
+++ avyBuddy/src/frontend/TextBasedMainloop.java
@@ -0,0 +1,14 @@
+package frontend;
+
+import uacData.DataToStringConversions;
+import uacData.Route;
+
+public class TextBasedMainloop {
+
+	public static void main(String[] args) {
+		Route route = new Route("salt-lake", "Grizzly Gulch", 0, 2);
+		
+		System.out.println(route);
+	}
+
+}
---
+++ avyBuddy/src/uacData/MainTest.java
@@ -0,0 +1,10 @@
+package uacData;
+
+import java.io.IOException;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		
+	}
+}
