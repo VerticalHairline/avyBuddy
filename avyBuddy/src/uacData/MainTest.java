9cac5c1c5da176ab6b55bbf9914679c967a7f4c6
Aiden Pickett <Ka7nqk@gmail.com>
2024-10-28 13:07:00 -0600

    Made functional sidebar as well as some placeholder html files that will soon have functionality. also showed a friend how the backend works so a new test is there as well

--- avyBuddy/src/uacData/MainTest.java
+++ avyBuddy/src/uacData/MainTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class MainTest {
 	
 	public static void main(String[] args) {
+		RouteDatabase.addRoute(new Route("salt-lake", "Superior", 0, 4, 5));
 		System.out.println(RouteDatabase.getRoute("Grizzly Gulch").getName());
 	}
 }
